7fd17a71eedac5909b047607d0e02779c41c32c7
ShreshthAwadhiya <ShreshthAwadhiya@192.168.2.122>
2018-06-12 17:21:48 -0400

    hi

--- Documents/workspace/oca/src/oca/basics/dayofWeek.java
+++ Documents/workspace/oca/src/oca/basics/dayofWeek.java
@@ -7,9 +7,8 @@ public static void main (String [] args){
 	int day = in.nextInt();
 	System.out.println("How many days from now do you want to know about? ");
 	int fut = in.nextInt();
-	int max = 6;
-	int min = 0;
-	int range = (max - min) +1;
+	int date = (day + fut) % 7 ;
+	
 	switch (day) {
 	
 	case (0):
@@ -34,5 +33,20 @@ public static void main (String [] args){
 		System.out.println("Saturday");
 	break;
 }
+	if (date == 0) {
+		System.out.printf("Todays is %s and the future day is Sunday", day);
+    }else if(date == 1){
+    	System.out.printf("Todays is %s and the future day is Monday", day);
+    }else if(date == 2){
+    	System.out.printf("Todays is %s and the future day is Tuesday", day);
+    }else if(date == 3){
+    	System.out.printf("Todays is %s and the future day is Wednesday", day);
+    }else if(date == 4){
+    	System.out.printf("Todays is %s and the future day is Thursday", day);
+    }else if(date == 5){
+    	System.out.printf("Todays is %s and the future day is Friday", day);
+    }else if(date == 6){
+    	System.out.printf("Todays is %s and the future day is Saturday", day);
+    }
 }
 }
--- Documents/workspace/oca/src/oca/basics/inRec.java
+++ Documents/workspace/oca/src/oca/basics/inRec.java
@@ -1,5 +1,21 @@
 package oca.basics;
+import java.util.Scanner;
 
 public class inRec {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 
+		
+		System.out.print("Enter a point: ");
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+
+		
+		boolean within = (Math.pow(Math.pow(x, 2), 0.5) <= 10 / 2 ) ||
+										  (Math.pow(Math.pow(y, 2), 0.5) <= 5.0 / 2);
+
+		
+		System.out.println("Point (" + x + ", " + y + ") is " +
+			((within) ? "in " : "not in ") + "the rectangle");
+	}
 }

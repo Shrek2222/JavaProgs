d4a73feaac5238e91936fcbac72ffaa96832b658
ShreshthAwadhiya <ShreshthAwadhiya@192.168.2.122>
2018-06-12 13:22:25 -0400

    update

---
+++ Documents/workspace/oca/src/oca/basics/TriangleSum2.java
@@ -0,0 +1,36 @@
+package oca.basics;
+import java.util.Scanner;
+public class TriangleSum2 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter three edges for a triangle:");
+		System.out.print(" (x1, y1) : ");
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		System.out.print("  x2, y2 : ");
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+		System.out.print("  x3, y3 : ");
+		double x3 = input.nextDouble();
+		double y3 = input.nextDouble();
+
+	
+		boolean valid = (x1 + y1 > x3 + y3 && x2 + y2 > x3 + y3) ||
+							 (x1 + y1 > x2 + y2 && x3 + y3 > x2 + y2) ||
+							 (x3 + y3 > x1 + y1 && x2 + y2 > x1 + y1);
+
+		if (!valid)
+		{
+			System.out.println("Triangle not invalid.");
+		}
+
+		
+		double side1 = Math.pow(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2), 0.5);
+		double side2 = Math.pow(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2), 0.5);
+		double side3 = Math.pow(Math.pow(x1 - x3, 2) + Math.pow(y1 - y3, 2), 0.5);
+
+		
+		System.out.println("perimeter of triangle is " + (side1 + side2 + side3));
+	}
+
+}

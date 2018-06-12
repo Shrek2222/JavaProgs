5daf62537e45efdedc75d58b5a4c722d71205be2
ShreshthAwadhiya <ShreshthAwadhiya@192.168.2.122>
2018-06-12 14:06:03 -0400

    update

--- Documents/workspace/oca/src/oca/basics/InCircle.java
+++ Documents/workspace/oca/src/oca/basics/InCircle.java
@@ -1,5 +1,21 @@
 package oca.basics;
 
-public class InCircle {
+import java.util.Scanner;
 
+public class InCircle {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+
+		
+		System.out.print("Enter two coordinates: ");
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+
+		
+		boolean within = (Math.pow(Math.pow(x, 2) + Math.pow(y, 2), 0.5) <= 10);
+
+		
+		System.out.println("Point (" + x + ", "+ y + ") is " + 
+			((within) ? "in " : "not in ") + "the circle"); 
+	}
 }
